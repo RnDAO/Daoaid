b90b625c8ecd5aeb730f049e4fb5127c5260ec1a
techyNonso <williamikeji@gmail.com>
2023-06-09 17:01:40 +0100

    problem listing done

--- Frontend/src/modules/Shared/services/api/problemApi.ts
+++ Frontend/src/modules/Shared/services/api/problemApi.ts
@@ -1,3 +1,11 @@
-export const getProblemsList = () => {
-  console.log("problem list");
+import axiosInstance from "@modules/Shared/lib/axiosInstance";
+
+export const getProblemsList = async () => {
+  const res = await axiosInstance({
+    // url of the api endpoint (can be changed)
+    url: "problems/",
+    method: "GET",
+  });
+
+  return res;
 };
